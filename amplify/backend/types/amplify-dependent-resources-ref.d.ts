9891b6acaf968f0d6e76f7c5487a433c1e52aca4
EC2 Default User <ec2-user@ip-172-31-31-206.eu-west-1.compute.internal>
2024-03-10 20:04:33 +0000

    Latest change

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyreactapp676a07cd": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3amplifyreactappstoragemukakhulu": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,15 +1,118 @@
 import logo from './logo.svg';
 import './App.css';
+import "@aws-amplify/ui-react/styles.css";
+//import AWS from 'aws-sdk';
+import { uploadData } from 'aws-amplify/storage';
 
-function App() {
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
+import { useState } from "react";
+Amplify.configure(awsExports);
+
+function App({ signOut}) {
+
+  const [fileData, setFileData] = useState(null);
+  const [fileStatus, setFileStatus] = useState(false);
+
+// upload file //
+
+  async function uploadFile(){
+    try {
+      const result = await uploadData({
+        key: fileData.name,
+        data: fileData,
+        options: {
+            accessLevel: 'private'
+        }
+      }).result;
+      setFileStatus(true)
+     // console.log('Succeeded: ', result);
+
+    } catch (error) {
+      console.log('Error : ', error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Weclome to hulisani app</h1>
-      </header>
-    </div>
+
+    <View className="App">
+
+       <Card>
+        <Image src={logo} className="App-logo" alt="logo" height="20%" width="20%"/>
+
+        <Heading level={1}> Welcome to Mulakhulu!</Heading>
+        
+      </Card>
+
+      <div>
+        <input type="file" accept={[".csv",".docx",".txt"]} onChange={(e)=> setFileData(e.target.files[0])}/>
+        </div>
+        
+        <div>
+          <button onClick={uploadFile} width="320" height="320">Upload File</button>
+        </div>
+
+      {fileStatus ? "File uploaded successfully" : ""}
+
+      <Button onClick={signOut} width="320" height="320">Log out</Button>
+
+    </View>
   );
+
 }
 
-export default App;
+export default withAuthenticator(App);
+
+
+
+/*
+function App({ signOut, user }) {
+
+ 
+
+  const [fileData, setFileData] = useState();
+  const [fileStatus, setFileStatus] = useState(false);
+
+  const uploadFile = async () => {
+    const result = await Storage.put(fileData.name, fileData, {
+      contentType: fileData.type,
+    });
+    setFileStatus(true);
+    console.log(21, result);
+  };
+
+  return (
+
+    <div className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" height="20%" width="20%"/>
+        <Heading level={1}> Welcome to Mulakhulu!</Heading>
+      </Card>
+      
+      <div>
+        <input type="file" onChange={(e) => setFileData(e.target.files[0])} />
+      </div>
+      <div>
+        <button onClick={uploadFile}>Upload file</button>
+      </div>
+      {fileStatus ? "File uploaded successfully" : ""}
+
+
+      <Button onClick={signOut} width="320" height="320">Log out</Button>
+
+    </div>
+
+  );
+
+}
+export default withAuthenticator(App);
+
+*/
