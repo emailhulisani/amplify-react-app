8c7981d55edb88d37e138dce36748459d4fd0d3a
EC2 Default User <ec2-user@ip-172-31-31-206.eu-west-1.compute.internal>
2024-03-11 20:37:34 +0000

    This release

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import "@aws-amplify/ui-react/styles.css";
 //import AWS from 'aws-sdk';
-import { uploadData,list } from 'aws-amplify/storage';
+import { uploadData ,list} from 'aws-amplify/storage';
 
 import {
   withAuthenticator,
@@ -11,16 +11,37 @@ import {
   Image,
   View,
   Card,
-  Input
 } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
+import { useState ,useEffect } from "react";
 
-import { useState } from "react";
+Amplify.configure(awsExports);
 
 function App({ signOut}) {
 
   const [fileData, setFileData] = useState(null);
   const [fileStatus, setFileStatus] = useState(false);
+  const [objects, setObjects] = useState([]);
+ 
+ useEffect(() => {
+   listObjects();
+ }, []);
+
+ async function listObjects(){
+  try {
+    const objectList = await list({
+      
+      options: {
+        accessLevel: 'private',
+      }
+    });
+    setObjects(objectList.items);
+    
+  } catch (error) {
+    console.log('Error ', error);
+  }
+}
 
 // upload file //
 
@@ -34,108 +55,47 @@ function App({ signOut}) {
         }
       }).result;
       setFileStatus(true)
-     // console.log('Succeeded: ', result);
+      listObjects();
+     
 
     } catch (error) {
       console.log('Error : ', error);
     }
   }
-
-/* Listing 
-async function listObject() {
-  
-  try {
-  const response = await list({
-    prefix: 'private/',
-    options: {
-      listAll: true
-    }
-  });
-  // render list items from response.items
-} catch (error) {
-  console.log('Error ', error);
-}}
-  
-//Call list
-
-
-useEffect(() => {
-  listObjects();
-}, []);
-*/
-
-  return (
-
-    <View className="App">
-
-       <Card>
-        <Image src={logo} className="App-logo" alt="logo" height="20%" width="20%"/>
-
-        <Heading level={1}> Welcome to Mulakhulu!</Heading>
-        
-      </Card>
-
-      <div>
-        <Input type="file" accept={[".csv",".docx",".txt"]} onChange={(e)=> setFileData(e.target.files[0])}/>
-        </div>
-        
-        <div>
-          <Button onClick={uploadFile} width="320" height="320">Upload File</Button>
-        </div>
-
-      {fileStatus ? "File uploaded successfully" : ""}
-
-      <Button onClick={signOut} width="320" height="320">Log out</Button>
-
-    </View>
-  );
-
-}
-
-export default withAuthenticator(App);
-
-
-
-/*
-function App({ signOut, user }) {
-
- 
-
-  const [fileData, setFileData] = useState();
-  const [fileStatus, setFileStatus] = useState(false);
-
-  const uploadFile = async () => {
-    const result = await Storage.put(fileData.name, fileData, {
-      contentType: fileData.type,
-    });
-    setFileStatus(true);
-    console.log(21, result);
-  };
-
   return (
 
     <div className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" height="20%" width="20%"/>
-        <Heading level={1}> Welcome to Mulakhulu!</Heading>
-      </Card>
-      
-      <div>
-        <input type="file" onChange={(e) => setFileData(e.target.files[0])} />
-      </div>
-      <div>
-        <button onClick={uploadFile}>Upload file</button>
-      </div>
-      {fileStatus ? "File uploaded successfully" : ""}
 
+
+<Card>
+        <Image src={logo} className="App-logo" alt="logo" height="20%" width="20%"/>
+
+        <Heading level={1}> Welcome to kuvuki land!</Heading>
+        
+      </Card>
+ 
+      <div>
+        <input type="file" accept={[".csv"]} onChange={(e)=> setFileData(e.target.files[0])}/>
+        </div>
+        
+        <div>
+          <button onClick={uploadFile} width="320" height="320">Upload File</button>
+        </div>
+
+      {fileStatus ? "File uploaded successfully" : ""}
+      <div>
+        {Array.prototype.map.call(objects, (x,i) => (
+          <div className="row" key={i}>{x.key}</div>
+        ))}
+      </div>
 
       <Button onClick={signOut} width="320" height="320">Log out</Button>
 
     </div>
-
   );
 
 }
+
+
 export default withAuthenticator(App);
 
-*/
